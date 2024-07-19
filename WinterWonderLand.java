3413424d63640e1754b6093f0d42536b11c14f80
SAVVY1710 <163214365+SAVVY1710@users.noreply.github.com>
2024-07-19 01:09:12 -0700

    Finished half the logic

--- WinterWonderLand.java
+++ WinterWonderLand.java
@@ -26,7 +26,6 @@ public class WinterWonderLand extends JFrame
 
     public static void main (String [] args)
     {
-
         WinterWonderLand JoeBobKim = new WinterWonderLand ();
     }
 
@@ -35,6 +34,7 @@ class Panel extends JPanel
 {
     int num;
     int num2;
+    //Constructor to initialize variables
     public Panel()
     {
         setBackground(Color.pink);
@@ -55,17 +55,55 @@ class Panel extends JPanel
 
     public void paintComponent(Graphics g)
     {
-        
+        if(num>=1)
+        {
+            int xn = 55;
+            int yn = 260;
+            Snowmen(g, xn, yn);
+
+            for(int i = 0; i < num-1; i++)
+            {
+                xn += 150;
+                Snowmen(g, xn, yn);
+            }
+        }
     }
     public void Stars(Graphics g2, int [] starx, int[]stary )	//passes in the graphics variable
     {
-        
-
+        g2.setColor(Color.WHITE);
+        g2.fillPolygon(starx, stary,12);
     }
     public void drawMoon(Graphics g3)	//passes in the graphics variable
     {
+        g3.setColor(Color.BLACK);
+        ((Graphics2D)g3).setStroke(new BasicStroke(3));
+        g3.drawArc(480, 0, 180, 150, 90, -180);
+        g3.setColor(Color.WHITE);
+        g3.fillArc(480, 0, 180, 150, 90, -180);
+
+        g3.setColor(Color.BLACK);
+        g3.drawArc(520, 0, 90, 150, 90, -180);
+        g3.setColor(Color.PINK);
+        g3.fillArc(520, 0, 90, 150, 90, -180);
     }
     public void Snowmen(Graphics g, int xn, int yn)
     {
+        g.setColor(Color.WHITE);
+        g.fillOval(xn, yn, 50, 40 );
+        g.fillOval(xn-5, yn+38, 60, 62 );
+        g.fillOval(xn-5, yn+99, 60, 62 );
+        g.fillOval(xn-20, yn+159, 90, 62 );
+        g.fillRect(0, 480, 800, 320);
+
+        g.setColor(Color.BLACK);
+        g.fillOval(xn + 10, yn + 10, 10, 10);
+        g.fillOval(xn + 30, yn + 10, 10, 10);
+        g.drawLine(xn +20, yn + 25, xn + 30, yn +25);
+        g.fillOval(xn + 20, yn + 50, 10, 10);
+        g.fillOval(xn + 20, yn + 65, 10, 10);
+        g.fillOval(xn + 20, yn + 80, 10, 10);
+
+        g.drawLine(xn+55, yn + 65, xn + 85, yn +110);
+        g.drawLine(xn-5, yn + 65, xn -35, yn +110);
     }
 }
